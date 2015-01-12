39b49021bba2641fd0d727a36472740a67a708f7
Douglas Teixeira <Doug@Douglass-MacBook-Pro.local>
2015-01-12 12:58:03 -0500

    Adds application_controller and sessions_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,60 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # before_action :set_testimonials, :set_facebook_feed
+  
+  helper_method :current_user, :current_user_is_admin? ,:permitted_params, :signed_in?
+
+  private
+  
+  def set_facebook_feed
+    user = User.find_by_credentials(ENV["DEV_EMAIL"], ENV["DEV_PASSWORD"])
+    @feed ||= user.facebook.get_connection(ENV["FB_PAGE_NAME"], "feed", {limit: 15}) if user && user.valid_facebook?
+  end
+  
+  # def set_testimonials
+#     @testimonials ||= Testimonial.all_display
+#   end
+  
+  def set_return_url
+    session[:return_url] = request.referrer
+  end
+
+  def current_user
+    return nil unless session[:token]
+    @current_user ||= User.find_by_session_token(session[:token])
+  end
+
+  def current_user_is_admin?
+    current_user && current_user.is_admin?
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:token] = user.reset_session_token!
+  end
+
+  def sign_out!
+    current_user.try(:reset_session_token!)
+    session[:token] = nil
+  end
+
+  def require_signed_in!
+    redirect_to new_session_url unless signed_in?
+  end
+
+  def require_signed_out!
+    redirect_to users_url if signed_in?
+  end
+
+  def password_confirmed?
+    params[:password] != params[:user][:password]
+  end
+
+  def permitted_params
+    @permitted_params ||= PermittedParams.new(params, current_user)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,53 @@
 class SessionsController < ApplicationController
+  before_filter :require_signed_out!, :set_return_url, :only => [:new]
+  before_filter :require_signed_in!, :only => [:update_facebook_auth, :destroy]
+  
+  def new
+    respond_to do |format|
+      format.html{ render :new }
+      # format.js { render :new }
+    end
+  end
+
+  def create
+    google_data = request.env["omniauth.auth"]
+    
+    if google_data
+      @user = User.where(provider: google_data["provider"], uid: google_data["uid"]).first
+      
+      @user = create_from_google_data(google_data) unless @user
+    else
+      @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password])
+    end
+
+    if @user
+      sign_in(@user)
+      flash[:notices] = ["Welcome #{@user.name}"]
+      fail
+      session[:return_url] = request.referrer unless session[:return_url]
+      redirect_to :back# session[:return_url]
+    else
+      flash.now[:errors] = ["Incorrect credentials"]
+      render :new
+    end
+  end
+  
+  def update_facebook_auth
+    current_user.update_omniauth!(env["omniauth.auth"])
+    
+    if current_user.valid?
+      flash[:notices] = ["Facebook Credentials Added!"]
+    else
+      flash[:errors] = ["An Error Has Occurred!"] + current_user.errors.full_messages
+    end
+    
+    redirect_to edit_user_url(current_user)
+  end
+
+  def destroy
+    sign_out!
+    redirect_to request.referrer
+  end
 end
